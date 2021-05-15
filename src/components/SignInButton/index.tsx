f7b09d795d517db927ba0519018be4ab8cebd6ec
Erick Malta <erickmalta100@gmail.com>
2021-05-15 19:37:55 -0300

    feat(SignInButton): implement signIn and signOut with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,24 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'; 
 
 import styles from './styles.module.scss';
 
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#04d361" />
-        ElladanTasartir
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     )
@@ -23,6 +27,7 @@ export function SignInButton() {
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
       >
         <FaGithub color="#eba417" />
         Sign in with Github
